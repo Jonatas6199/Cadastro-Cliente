54d1d0486e45933feb33111571fe8c822f3d46dd
Jonatas João <jojokl@hotmail.com>
2022-06-10 01:26:17 -0300

    Add interfaces

---
+++ WebAPI/Interfaces/IRepositories/IClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using CadastroPersistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Interfaces.IRepositories
+{
+    public interface IClientRepository 
+    {
+        List<Client> GetClients();
+    }
+}
