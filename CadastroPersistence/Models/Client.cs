33012a5accd7075e9b86212ce65f56bb3133acbd
Jonatas João <jojokl@hotmail.com>
2022-06-10 01:25:26 -0300

    create persistence project for models and later for database management

---
+++ CadastroPersistence/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPersistence.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string CPF { get; set; }
+        public string Name { get; set; }
+        public string RG { get; set; }
+        public DateTime ShippingDate { get; set; }// Data da expedição
+        public string IssuingAgency { get; set; } //Órgão expedidor
+        public string UF { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Sex { get; set; }
+        public string MaritalStatus { get; set; } //Estado Civil
+    }
+}
---
+++ CadastroPersistence/Models/ClientAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPersistence.Models
+{
+    public class ClientAddress
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public string CEP { get; set; }
+        public string StreetName { get; set; }
+        public string Number { get; set; }
+        public string Complement { get; set; }
+        public string Neighbourhood { get; set; }
+        public string City { get; set; }
+        public string UF { get; set; }
+    }
+}
